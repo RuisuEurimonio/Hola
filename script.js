b87cf3d7a1e692d192ea5780e0982ec1f8f2dbae
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-09-18 01:30:22 -0500

    second commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const trackListItems = document.querySelectorAll('.track-list li');
 
 trackListItems.forEach(item => {
     item.addEventListener('click', () => {
+        trackListItems.forEach(el => el.classList.remove('active')); // Remover clase activa de todas las canciones
+        item.classList.add('active'); // Añadir clase activa a la canción seleccionada
+
         const src = item.getAttribute('data-src');
         audioSource.src = src;
         audio.load();
